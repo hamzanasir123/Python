8e86707daca3e6bc6b107a15c763e1eb9f1e2bed
Hamza Nasir <hk435981@gmail.com>
2025-02-17 11:15:24 +0500

    Functions Learned

---
+++ Functions.py
@@ -0,0 +1,38 @@
+# Functions
+
+# Block Of Statements That Perform A Specific Task
+
+def sum(a,b):
+    print(a + b)
+
+
+sum(2,3)
+
+def printHello():
+    print("Hello")
+
+printHello()
+
+
+# Average Of 3 Numbers
+
+def avg(a,b,c):
+    r = a + b + c
+    res = r / 3
+    print(res)
+    return res
+
+avg(23,45,56)
+
+
+# Built-in Functions & User Defined Functions
+
+# Built-in Functions
+
+# print()
+# len()
+# type()
+# range()
+
+# User Defined Functions
+
